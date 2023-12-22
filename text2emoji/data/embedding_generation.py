07a4faa21e7924455fcfdf1cdf648c901021be41
Jeremias Ferrao <feronest@yahoo.com>
2023-12-22 23:40:11 +0100

    Added tf-idf score calculation

--- text2emoji/data/embedding_generation.py
+++ text2emoji/data/embedding_generation.py
@@ -19,6 +19,27 @@ def invalid_text_vec(v_size):
     return np.zeros(v_size, dtype=np.float32)
 
 
+def calculate_idf_scores(df):
+    """
+    Get the IDF scores of the words in the given dataframe.
+    Only used with training data.
+
+    Args:
+        df (Dataframe): The dataframe which contains all the tweets.
+    """
+
+    # Tokenize each row and store the tokens in a list
+    tokenized_texts = [set(nltk.word_tokenize(row.text)) for row in tqdm(df.itertuples(), total=len(df), desc="Tokenizing texts")]
+
+    # Get all unique words
+    words = set.union(*tokenized_texts)
+
+    # Calculate IDF score for each word
+    idf_scores = {word: np.log(len(df) / sum([word in text for text in tokenized_texts])) for word in tqdm(words, desc="Calculating IDF scores")}
+
+    np.save("idf_scores.npy", idf_scores)
+
+
 def make_w2v_embeddings(df, name, w2v_model):
     """
     Create word2vec embeddings for the given dataframe.
@@ -29,13 +50,19 @@ def make_w2v_embeddings(df, name, w2v_model):
         w2v_model: Word2Vec model obtained from gensim.
 
     Returns:
-        _type_: _description_
+        DataFrame: The dataframe with the embeddings.
     """
 
     features = []
     v_size = w2v_model.vector_size
     n_invalid_texts = 0
     n_only_unknown_words = 0
+
+    # Calculate IDF scores if training data, otherwise load them
+    if name == "train":
+        calculate_idf_scores(df)
+
+    idf_scores = np.load("idf_scores.npy", allow_pickle=True).item()
 
     for row in tqdm(df.itertuples(), total=len(df), desc=name):
 
@@ -47,10 +74,22 @@ def make_w2v_embeddings(df, name, w2v_model):
             features.append(invalid_text_vec(v_size))
             continue
 
+        # Determine the frequency of each word in the sentence
+        tf_scores = {word: sentence.count(word) for word in sentence}
+
+        # Get the word vectors using word2vec and weights using tf-idf
         word_vectors = []
+        word_weights = []
         for word in sentence:
             try:
-                word_vectors.append(w2v_model[word])
+
+                # We use a temporary variable in case either the word embedding or the word weight is not found
+                word_embedding = w2v_model[word]
+                word_weight = idf_scores[word] * tf_scores[word]
+
+                word_vectors.append(word_embedding)
+                word_weights.append(word_weight)
+
             except KeyError:
                 # ignore (e.g. unknown word)
                 continue
@@ -61,7 +100,12 @@ def make_w2v_embeddings(df, name, w2v_model):
             continue
 
         word_vectors = np.array(word_vectors)
-        mean_vector = word_vectors.mean(axis=0)
+        word_weights = np.array(word_weights)
+
+        # Multiply each word vector with its weight and then take the mean
+        weighted_vectors = word_vectors * word_weights[:, None]
+        mean_vector = np.mean(weighted_vectors, axis=0)
+
         features.append(mean_vector)
 
     return np.array(features), n_invalid_texts, n_only_unknown_words
