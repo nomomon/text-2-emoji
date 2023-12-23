ea2dafa014b27543bb86a1a447c3f2a17fb310ac
Jeremias Ferrao <feronest@yahoo.com>
2023-12-23 02:07:22 +0100

    Added contractions

--- create_model.py
+++ create_model.py
@@ -11,7 +11,7 @@ def find_best_model(model_type):
         "n_neurons": [10, 50, 100],
         "optimizer_type": ["adam"],
         "learning_rate": [1e-1, 1e-2, 1e-3],
-        "epochs": [10, 20, 50, 100, 200],
+        "epochs": [80, 100, 125, 150, 175, 200],
     }
 
     grid_search_model = GridSearchModel(hyperparameters, model_type)
@@ -25,4 +25,4 @@ def find_best_model(model_type):
 if __name__ == "__main__":
     model_type = "word2vec"
     find_best_model(model_type)
-    eval_best_model(model_type, "valid")
+    # eval_best_model(model_type, "valid")
--- text2emoji/data/text_processing.py
+++ text2emoji/data/text_processing.py
@@ -78,6 +78,12 @@ def preprocess_text(text):
             str: Preprocessed text
     """
 
+    # Lowercase text
+    text = text.lower()
+
+    # Remove contractions
+    text = remove_contractions(text)
+
     # Clean text
     text = clean_text(text)
 
@@ -87,7 +93,138 @@ def preprocess_text(text):
     # Lemmatize text
     text = lemmatize_text(text)
 
-    # Lowercase text
-    text = text.lower()
+    # If text is null, replace it with a placeholder
+    if text == '' or not text:
+        text = 'Unknown'
+
+    return text
+
+
+def remove_contractions(text):
+    """
+        Remove contractions from text
+
+        Args:
+            text (str): Text to remove contractions from
+        Returns:
+            text (str): Text without contractions
+    """
+
+    # Contractions mapping
+    contractions = {
+        "ain't": "am not",
+        "aren't": "are not",
+        "can't": "cannot",
+        "can't've": "cannot have",
+        "'cause": "because",
+        "could've": "could have",
+        "couldn't": "could not",
+        "couldn't've": "could not have",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hadn't've": "had not have",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he'd": "he would",
+        "he'd've": "he would have",
+        "he'll": "he will",
+        "he'll've": "he will have",
+        "he's": "he is",
+        "how'd": "how did",
+        "how'd'y": "how do you",
+        "how'll": "how will",
+        "how's": "how is",
+        "i'd": "i would",
+        "i'd've": "i would have",
+        "i'll": "i will",
+        "i'll've": "i will have",
+        "i'm": "i am",
+        "i've": "i have",
+        "isn't": "is not",
+        "it'd": "it would",
+        "it'd've": "it would have",
+        "it'll": "it will",
+        "it'll've": "it will have",
+        "it's": "it is",
+        "let's": "let us",
+        "ma'am": "madam",
+        "mayn't": "may not",
+        "might've": "might have",
+        "mightn't": "might not",
+        "mightn't've": "might not have",
+        "must've": "must have",
+        "mustn't": "must not",
+        "mustn't've": "must not have",
+        "needn't": "need not",
+        "needn't've": "need not have",
+        "o'clock": "of the clock",
+        "oughtn't": "ought not",
+        "oughtn't've": "ought not have",
+        "shan't": "shall not",
+        "sha'n't": "shall not",
+        "shan't've": "shall not have",
+        "she'd": "she would",
+        "she'd've": "she would have",
+        "she'll": "she will",
+        "she'll've": "she will have",
+        "she's": "she is",
+        "should've": "should have",
+        "shouldn't": "should not",
+        "shouldn't've": "should not have",
+        "so've": "so have",
+        "so's": "so is",
+        "that'd": "that would",
+        "that'd've": "that would have",
+        "that's": "that is",
+        "there'd": "there would",
+        "there'd've": "there would have",
+        "there's": "there is",
+        "they'd": "they would",
+        "they'd've": "they would have",
+        "they'll": "they will",
+        "they'll've": "they will have",
+        "they're": "they are",
+        "they've": "they have",
+        "to've": "to have",
+        "wasn't": "was not",
+        " u ": " you ",
+        " ur ": " your ",
+        " n ": " and ",
+        "won't": "would not",
+        'dis': 'this',
+        'bak': 'back',
+        'brng': 'bring',
+        "'s ": " is ",
+        "'r ": " are ",
+        "'ve ": " have ",
+        "thats": "that is",
+        "wont": "will not",
+        "cant": "can not",
+        "cannot": "can not",
+        "youre": "you are",
+        "loveu": "love you",
+        " im": "i am",
+        " ill": "i will",
+        "cuz": "because",
+        "wanna": "want to",
+        "gonna": "going to",
+        "gotta": "got to",
+    }
+
+    # Replace contractions
+    text = text.replace("’", "'").replace("‘", "'").replace("´", "'") \
+        .replace("`", "'").replace("“", '"').replace("”", '"') \
+        .replace("„", '"').replace("«", '"').replace("»", '"') \
+        .replace("–", "-").replace("—", "-").replace("…", "...") \
+        .replace(" ", " ").replace("•", "*").replace("·", "*") \
+        .replace("‹", "<").replace("›", ">").replace("™", "TM") \
+        .replace("©", "(c)").replace("®", "(r)").replace("°", " degrees") \
+        .replace("€", " euros").replace("$", " dollars").replace("£", " pounds") \
+        .replace("₤", " pounds").replace("×", "x").replace("²", "2")
+
+    for contraction in contractions:
+        text = text.replace(contraction, contractions[contraction])
 
     return text
