f694d92a90b86dde976f3b24f0a206bcc519fd4d
nomomon <funisinfinity26@gmail.com>
2024-01-09 11:26:43 +0600

    Add EmojiPrediction and QueryResponse models, update get_emoji endpoint

--- api.py
+++ api.py
@@ -2,6 +2,18 @@ import numpy as np
 import pandas as pd
 from fastapi import FastAPI
 
+from typing import List
+from pydantic import BaseModel
+
+class EmojiPrediction(BaseModel):
+    label: int
+    emoji: str
+    name: str
+    probability: float
+
+class QueryResponse(BaseModel):
+    results: List[EmojiPrediction]
+
 app = FastAPI()
 
 
@@ -11,13 +23,18 @@ async def root():
 
 
 @app.get("/get_emoji")
-async def get_emoji(text: str):
+async def get_emoji(text: str) -> QueryResponse:
+    """
+    Returns a list of emoji predictions for the given text. The list is sorted by probability. And the probability sum up to 1.
+    """
+
     probs = get_emoji(text)
 
     labels = pd.read_csv('./data/bronze/mapping.csv')
     labels["probability"] = probs
+    labels = labels.sort_values(by="probability", ascending=False)
 
-    return labels.to_dict(orient='records')
+    return QueryResponse(results=labels.to_dict(orient="records"))
 
 def get_emoji(text: str):
     a = np.random.rand(20)
