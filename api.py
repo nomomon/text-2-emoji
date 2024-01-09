991f2c736b276b6fc1acc00451bbd5974632727f
nomomon <funisinfinity26@gmail.com>
2024-01-09 11:23:49 +0600

    Add emoji probability calculation and mapping

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,6 +9,16 @@ app = FastAPI()
 async def root():
     return {"status": "ok"}
 
+
 @app.get("/get_emoji")
 async def get_emoji(text: str):
-    return {"emoji": text}
+    probs = get_emoji(text)
+
+    labels = pd.read_csv('./data/bronze/mapping.csv')
+    labels["probability"] = probs
+
+    return labels.to_dict(orient='records')
+
+def get_emoji(text: str):
+    a = np.random.rand(20)
+    return a / a.sum()
