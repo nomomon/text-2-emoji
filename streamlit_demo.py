734a4c7f93394518e0c90f3a066026d64ef03fb6
ExclBrick <ExclBrick@github.com>
2024-01-16 00:34:01 +0100

    Data now presented in table. Background color of probability depends on its value. RequestException yields a message in streamlit informing the user of the error

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import requests
+import pandas as pd
+from matplotlib.colors import LinearSegmentedColormap
+
 
 def handle_streamlit():
-    title = "Emoji prediction"
+    """
+    Creates a streamlit frontend. Receives a json file from the API
+    containing emojis and probabilities, and presents them in a table.
+
+    Returns:
+         None
+    """
+    title = "Text-2-Emoji"
     st.write(title)
 
     usr_sentence = st.text_input("Please enter a sentence:")
@@ -11,15 +21,17 @@ def handle_streamlit():
     usr_params = {'text': usr_sentence}
     try:
         data = requests.get(api_url, params=usr_params).json()
-    except:
-        st.write("API request failed")
+    except requests.exceptions.RequestException as e:
+        st.write(f"API request failed. Exception type: {type(e).__name__}")
         return
 
-    # TODO: Put the output in a table
-    for ix, result in enumerate(data["results"]):
-        emoji = result["emoji"]
-        prob = result["probability"]
-        st.write(f"Number {ix}: \t{emoji}. \tProbability {prob}")
+    df_data = pd.json_normalize(data["results"])
+
+    custom_darkcyan = ["#18191A", "darkcyan"]    # Probability background color based on its value
+    custom_cmap = LinearSegmentedColormap.from_list("mycmap",custom_darkcyan)
+    df_data = df_data.style.background_gradient(axis=0, subset="probability",
+                                                vmin=0, vmax=1, cmap=custom_cmap)
+    st.table(df_data)
 
 
 if __name__ == '__main__':
